e12d80d7906314317d3dc9a268bc6574125f0ab4
eilon <eilon159123@gmail.com>
2023-01-11 12:05:51 +0200

    finish myserver

--- src/test/MyServer.java
+++ src/test/MyServer.java
@@ -1,7 +1,52 @@
 package test;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class MyServer {
+    private int port;
+    private ClientHandler ch;
+    private volatile boolean stop;
 
-	
+    public MyServer(int port, ClientHandler ch) {
+        this.port = port;
+        this.ch = ch;
+        stop = false;
+    }
+
+    private void runServer() throws Exception {
+        ServerSocket server = new ServerSocket(port);
+        server.setSoTimeout(1000);
+        while (!stop) {
+            try {
+                Socket aClient = server.accept(); // blocking call
+                try {
+                    ch.handleClient(aClient.getInputStream(), aClient.getOutputStream());
+                    aClient.getInputStream().close();
+                    aClient.getOutputStream().close();
+                    aClient.close();
+                } catch (IOException e) {
+                    /* ... */}
+            } catch (SocketTimeoutException e) {
+                /* ... */}
+        }
+        server.close();
+    }
+
+    public void start() {
+        new Thread(() -> {
+            try {
+                runServer();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void close() {
+        stop = true;
+    }
+
 }
