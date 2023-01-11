56640875ac0cd0b17aa692c4e828ce3073574fdd
eilon <eilon159123@gmail.com>
2023-01-11 12:57:29 +0200

    finish dm

--- src/test/DictionaryManager.java
+++ src/test/DictionaryManager.java
@@ -1,6 +1,62 @@
 package test;
 
+import java.util.HashMap;
 
 public class DictionaryManager {
+    private HashMap<String, Dictionary> dictionaries;
+    private static DictionaryManager instance = null;
+
+    private DictionaryManager() {
+        dictionaries = new HashMap<>();
+    }
+
+    public static DictionaryManager get() {
+        if (instance == null) {
+            instance = new DictionaryManager();
+        }
+        return instance;
+    }
+
+    public int getSize() {
+        return dictionaries.size();
+    }
+
+    public boolean query(String... args) {
+        if (args.length < 1) {
+            return false;
+        }
+        String searchWord = args[args.length - 1];
+        boolean result = false;
+        for (int i = 0; i < args.length - 1; i++) {
+            if (!dictionaries.containsKey(args[i])) {
+                addDictionary(args[i], new Dictionary(args[i]));
+            }
+            if (dictionaries.get(args[i]).query(searchWord)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public boolean challenge(String... args) {
+        if (args.length < 1) {
+            return false;
+        }
+        String searchWord = args[args.length - 1];
+        boolean result = false;
+        for (int i = 0; i < args.length - 1; i++) {
+            if (!dictionaries.containsKey(args[i])) {
+                addDictionary(args[i], new Dictionary(args[i]));
+            }
+            if (dictionaries.get(args[i]).challenge(searchWord)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private void addDictionary(String name, Dictionary dictionary) {
+        dictionaries.put(name, dictionary);
+    }
 
 }
--- src/test/MainTrain.java
+++ src/test/MainTrain.java
@@ -1,7 +1,7 @@
 package test;
 
-// import java.io.FileWriter;
-// import java.io.IOException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -77,55 +77,67 @@ public class MainTrain {
 		if (Thread.activeCount() != c) {
 			System.out.println("you have a thread open after calling close method (-100)");
 			ok = false;
-		}
-		else{
+		} else {
 			System.out.println("you closed all threads after calling close method");
 		}
 		return ok;
 	}
 
-	// public static String[] writeFile(String name) {
-	// Random r=new Random();
-	// String txt[]=new String[10];
-	// for(int i=0;i<txt.length;i++)
-	// txt[i]=""+(10000+r.nextInt(10000));
+	public static String[] writeFile(String name) {
+		Random r = new Random();
+		String txt[] = new String[10];
+		for (int i = 0; i < txt.length; i++)
+			txt[i] = "" + (10000 + r.nextInt(10000));
 
-	// try {
-	// PrintWriter out=new PrintWriter(new FileWriter(name));
-	// for(String s : txt) {
-	// out.print(s+" ");
-	// }
-	// out.println();
-	// out.close();
-	// }catch(Exception e) {}
+		try {
+			PrintWriter out = new PrintWriter(new FileWriter(name));
+			for (String s : txt) {
+				out.print(s + " ");
+			}
+			out.println();
+			out.close();
+		} catch (Exception e) {
+		}
 
-	// return txt;
-	// }
+		return txt;
+	}
 
-	// public static void testDM() {
-	// String t1[]=writeFile("t1.txt");
-	// String t2[]=writeFile("t2.txt");
-	// String t3[]=writeFile("t3.txt");
+	public static void testDM() {
+		String t1[] = writeFile("t1.txt");
+		String t2[] = writeFile("t2.txt");
+		String t3[] = writeFile("t3.txt");
 
-	// DictionaryManager dm=DictionaryManager.get();
+		DictionaryManager dm = DictionaryManager.get();
 
-	// if(!dm.query("t1.txt","t2.txt",t2[4]))
-	// System.out.println("problem for Dictionary Manager query (-5)");
-	// if(!dm.query("t1.txt","t2.txt",t1[9]))
-	// System.out.println("problem for Dictionary Manager query (-5)");
-	// if(dm.query("t1.txt","t3.txt","2"+t3[2]))
-	// System.out.println("problem for Dictionary Manager query (-5)");
-	// if(dm.query("t2.txt","t3.txt","3"+t2[5]))
-	// System.out.println("problem for Dictionary Manager query (-5)");
-	// if(!dm.challenge("t1.txt","t2.txt","t3.txt",t3[2]))
-	// System.out.println("problem for Dictionary Manager challenge (-5)");
-	// if(dm.challenge("t2.txt","t3.txt","t1.txt","3"+t2[5]))
-	// System.out.println("problem for Dictionary Manager challenge (-5)");
-
-	// if(dm.getSize()!=3)
-	// System.out.println("wrong size for the Dictionary Manager (-10)");
-
-	// }
+		if (!dm.query("t1.txt", "t2.txt", t2[4]))
+			System.out.println("problem for Dictionary Manager query (-5) 1");
+		else
+			System.out.println("query 1 passed");
+		if (!dm.query("t1.txt", "t2.txt", t1[9]))
+			System.out.println("problem for Dictionary Manager query (-5) 2");
+		else
+			System.out.println("query 2 passed");
+		if (dm.query("t1.txt", "t3.txt", "2" + t3[2]))
+			System.out.println("problem for Dictionary Manager query (-5) 3");
+		else
+			System.out.println("query 3 passed");
+		if (dm.query("t2.txt", "t3.txt", "3" + t2[5]))
+			System.out.println("problem for Dictionary Manager query (-5) 4");
+		else
+			System.out.println("query 4 passed");
+		if (!dm.challenge("t1.txt", "t2.txt", "t3.txt", t3[2]))
+			System.out.println("problem for Dictionary Manager challenge (-5) 5");
+		else
+			System.out.println("challenge 1 passed");
+		if (dm.challenge("t2.txt", "t3.txt", "t1.txt", "3" + t2[5]))
+			System.out.println("problem for Dictionary Manager challenge (-5) 6");
+		else
+			System.out.println("challenge 2 passed");
+		if (dm.getSize() != 3)
+			System.out.println("wrong size for the Dictionary Manager (-10)");
+		else
+			System.out.println("Dictionary Manager size is correct");
+	}
 
 	// public static void runClient(int port,String query,boolean result) {
 	// try {
@@ -163,11 +175,11 @@ public class MainTrain {
 	// }
 
 	public static void main(String[] args) {
-		testServer();
-		// if(testServer()) {
-		// testDM();
-		// testBSCH();
-		// }
+		// testServer();
+		if (testServer()) {
+			testDM();
+			// testBSCH();
+		}
 		System.out.println("done");
 	}
 
