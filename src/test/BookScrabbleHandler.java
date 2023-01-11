9e056f120f1d281346ee4be9933860d68432309d
eilon <eilon159123@gmail.com>
2023-01-11 16:34:49 +0200

    add some documentation

--- src/test/BookScrabbleHandler.java
+++ src/test/BookScrabbleHandler.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class BookScrabbleHandler implements ClientHandler {
 
-    DictionaryManager dm;
+    DictionaryManager dm;// = DictionaryManager.get();
 
     public BookScrabbleHandler() {
         dm = DictionaryManager.get();
@@ -20,22 +20,29 @@ public class BookScrabbleHandler implements ClientHandler {
         try {
             Scanner in = new Scanner(inFromclient);
             PrintWriter out = new PrintWriter(outToClient);
-            String[] args = in.next().split(",");
+
+            String[] args = in.next().split(",");// get the input from the client
+
             ArrayList<String> argsList = new ArrayList<String>(Arrays.asList(args));
+            // remove the first element of the array (the command) for the query and
+            // challenge methods
             argsList.remove(0);
-            boolean result = false;
+
+            boolean result = false;// the result of the query or challenge
+
             if (!args[0].equals("Q") && !args[0].equals("C")) {
+                // not a valid command
                 out.println("ERROR");
             }
 
             else if (args[0].equals("Q")) {
                 String[] query = argsList.toArray(new String[argsList.size()]);
-                result = dm.query(query);
+                result = dm.query(query);// query the dictionary manager with the query string array
             }
 
             else if (args[0].equals("C")) {
                 String[] challenge = argsList.toArray(new String[argsList.size()]);
-                result = dm.challenge(challenge);
+                result = dm.challenge(challenge);// challenge the dictionary manager with the challenge string array
             }
 
             if (result) {
@@ -44,9 +51,9 @@ public class BookScrabbleHandler implements ClientHandler {
                 out.println("false");
             }
 
-            out.flush();
-            in.close();
-            out.close();
+            out.flush(); // flush the output stream
+            in.close(); // close the input stream
+            out.close(); // close the output stream
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
